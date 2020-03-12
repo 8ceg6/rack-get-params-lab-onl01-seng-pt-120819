6a3f4e11ffcaab63c57b71b2e32437f5a52fba1a
cedric e goddard <8ceg6@comcast.net>
2020-03-12 16:53:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -21,6 +21,13 @@ class Application
           resp.write "#{item}\n"
         end 
       end 
+    elsif req.path.match(/add/)
+      search_term = req.params["items"]
+      if @@items.include?(search_term)
+        @@cart << search_term
+        resp.write "added #{search_term}"
+      else resp.write "We don't have that item"
+      end 
     else
       resp.write "Path Not Found"
     end
